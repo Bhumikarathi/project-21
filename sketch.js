dc67b0cf917f6e34ebe9e448c348f4cd331486ff
Bhumikarathi <77321482+Bhumikarathi@users.noreply.github.com>
2021-06-02 21:27:25 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,48 +1,76 @@
-var canvas;
-var music;
-var surface1,surface2,surface3,surface4;
-var gampu;
-function preload(){
-    music = loadSound("music.mp3");
+var helicopterIMG, helicopterSprite, packageSprite, packageIMG;
+var packageBody;
+var box1,box2,box3;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-function setup(){
-    canvas = createCanvas(800,600);
+	packageSprite=createSprite(width/2, 80, 20,20);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-surface1=createSprite(100,80,120,60);
-surface1.shapeColour="black";
-surface2=createSprite(200,80,120,60);
-surface2.shapeColour="black";
-surface3=createSprite(300,80,120,60);
-surface3.shapeColour="black";
-surface4=createSprite(400,80,120,60);
-surface4.shapeColour="black";
-    //create 4 different surfaces
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
-gampu=createSprite(40,40,100,100);
-gampu.velocityX=11;
-    //create box sprite and give velocity
 
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 20 , {restitution:0.2, isStatic:true});
+	World.add(world, packageBody);
+
+
+	
+
+	box2 = new Box(500,650,20,100,"red");
+	box3 = new Box(295,650,20,100,"red");
+	box1 = new Box(400,690,200,20,"red");
+	
+	Engine.run(engine);
 }
+
 
 function draw() {
-    background(rgb(169,169,169));
-    //create edgeSprite
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
 
-bounceOff(gampu,surface1);
+  if(keyDown(LEFT_ARROW)) {
+	helicopterSprite.x=helicopterSprite.x-20;
+	Matter.Body.translate(packageBody,{x:-20,y:0})
+	}	
+	
+if(keyDown(RIGHT_ARROW)) {
+	helicopterSprite.x=helicopterSprite.x+20;
+	Matter.Body.translate(packageBody,{x:+20,y:0})
+	}	
 
-    //add condition to check if box touching surface and make it box
+  if(keyDown(DOWN_ARROW)) {
+	Matter.Body.setStatic(packageBody,false);
+	}	
+	
+	box1.display();
+	box2.display();
+	box3.display();
+
+  Engine.update(engine);
+  
 }
-function bounceOff(object1,object2){
-    if (object1.x - object2.x < object2.width/2 + object1.width/2
-      && object2.x - object1.x < object2.width/2 + object1.width/2) {
-      object1.velocityX = object1.velocityX * (-1);
-      object2.velocityX = object2.velocityX * (-1);
-    }
-    if (object1.y - object2.y < object2.height/2 + object1.height/2
-      && object2.y - object2.y < object2.height/2 + object1.height/2) {
-        object1.velocityY = object1.velocityY * (-1);
-        object2.velocityY = object2.velocityY * (-1);
-    } 
-  }
